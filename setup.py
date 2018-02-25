34966b63f94ca4f789ed0b6a60bd5baf6b6f78aa
Alex Kuzmenko <alx.kuzm@gmail.com>
2018-02-25 14:34:53 +0200

    v0.0.1a2

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='yield2await',
-    version='0.0.1',
+    version='0.0.1a2',
     description='Transform your python code to use async/await (PEP 492)',
-    keywords='yield from async await async with',
+    keywords='yield from asyncio coroutines with async await',
     url='https://github.com/alxpy/yield2await',
     author='Oleksandr Kuzmenko',
     license='MIT',
@@ -14,10 +14,13 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=['astor==0.6.2'],
+    py_modules=['yield2await'],
+    python_requires='>=3.4',
     entry_points={
         'console_scripts': 'yield2await=yield2await:main',
     },
