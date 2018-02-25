eb11cc82a1fcebcd4bdcc8a3f759d5ba9a0bdcc3
Alex Kuzmenko <alx.kuzm@gmail.com>
2018-02-25 22:20:09 +0200

    add readme

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
 
 
+with open('requirements.txt') as f:
+    install_requires = f.read().split()
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setup(
     name='yield2await',
-    version='0.0.1a2',
+    version='0.0.1a3',
     description='Transform your python code to use async/await (PEP 492)',
+    long_description=long_description,
     keywords='yield from asyncio coroutines with async await',
     url='https://github.com/alxpy/yield2await',
     author='Oleksandr Kuzmenko',
@@ -14,13 +23,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=['astor==0.6.2'],
+    install_requires=install_requires,
     py_modules=['yield2await'],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     entry_points={
         'console_scripts': 'yield2await=yield2await:main',
     },
